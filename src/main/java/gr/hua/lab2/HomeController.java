b85ebea8184584974b54ac7fb864f8c5d30e567d
tsadimas <tsadimas@hua.gr>
2015-11-30 13:11:53 +0200

    Update HomeController.java

--- src/main/java/gr/hua/lab2/HomeController.java
+++ src/main/java/gr/hua/lab2/HomeController.java
@@ -22,43 +22,22 @@ import gr.hua.lab2.model.Employee;
  */
 @Controller
 public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		 ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 
-	    Employee emp = (Employee) context.getBean("employee");
-	      
-		emp.setName("Mitsos");
-		
-		 Employee emp1 = (Employee) context.getBean("employee");
-	      
-			emp1.setName("Mitsos1");
-		
-		String formattedDate = dateFormat.format(date);
-		
-		
-		EmployeeDAO employeeDAO = context.getBean("employeeDAO", EmployeeDAO.class);
-		
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+	ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+	// Get the EmployeeDAO Bean
+	EmployeeDAO employeeDAO = ctx.getBean("employeeDAO", EmployeeDAO.class);
+
+	@RequestMapping("/")
+	public String home(Model model) {
+
+		model.addAttribute("employee", new Employee());
 		List<Employee> empList = employeeDAO.getAll();
-		List<Employee> staff = employeeDAO.getStaff();
-		model.addAttribute("employees",	empList);
-		model.addAttribute("staff",	staff);
-		
-		model.addAttribute("serverTime", formattedDate );
-		model.addAttribute("emp", emp.getName() );
-		model.addAttribute("emp1", emp1.getName() );
-		
+		model.addAttribute("employees", empList);
 		return "home";
+
 	}
+
 	
 }
