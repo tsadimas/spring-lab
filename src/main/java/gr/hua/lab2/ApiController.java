7dc4c6c9da74f4ae9e8ed4c0802ba84a26ad2875
Anargyros Tsadimas <tsadimas@gmail.com>
2016-01-08 13:49:50 +0200

    added rest api

---
+++ src/main/java/gr/hua/lab2/ApiController.java
@@ -0,0 +1,62 @@
+package gr.hua.lab2;
+
+import java.util.List;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import gr.hua.lab2.model.Employee;
+import gr.hua.lab2.model.EmployeeDAO;
+
+
+@Controller
+public class ApiController {
+
+	ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+	// Get the EmployeeDAO Bean
+	EmployeeDAO employeeDAO = ctx.getBean("employeeDAO", EmployeeDAO.class);
+
+	
+	@RequestMapping(value = "/employees", method = RequestMethod.GET, produces = "application/json")
+    public @ResponseBody List<Employee> getEmployeesInJSON() {
+	List<Employee>employees=employeeDAO.getAll();
+   	 return employees;
+    }
+	
+	@RequestMapping(value = "/employee/{id}", method = RequestMethod.GET, produces = "application/json")
+	public @ResponseBody Employee getEmployeeInJSON(@PathVariable Integer id) throws EmployeeNotFoundException {
+	Employee emp=employeeDAO.getById(id);
+	
+	if (emp == null) {
+		throw new EmployeeNotFoundException();
+	}
+   	 return emp;
+    }
+	
+	@RequestMapping(value = "/employee/add", method = RequestMethod.POST, produces = "application/json")
+	@ResponseStatus(HttpStatus.CREATED) 
+	public @ResponseBody Employee insertEmployeeInJSON(@RequestBody Employee empl)  {
+	employeeDAO.save(empl);
+   	 return empl;
+    }
+	
+	
+	@RequestMapping(value = "/employee/addparam", method = RequestMethod.POST, produces = "application/json")
+	@ResponseStatus(HttpStatus.CREATED) 
+	public @ResponseBody Employee paraminsertEmployeeInJSON(@RequestParam("name") String name,@RequestParam("role") String role )  {
+	Employee empl=new Employee();
+	empl.setName(name);
+	empl.setRole(role);
+		employeeDAO.save(empl);
+	
+   	 return empl;
+    }
+}
---
+++ src/main/java/gr/hua/lab2/EmployeeNotFoundException.java
@@ -0,0 +1,11 @@
+package gr.hua.lab2;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "This employee is not found in the system")
+public class EmployeeNotFoundException extends Exception {
+	private static final long serialVersionUID = 100L;
+}
