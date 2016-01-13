e0dd2f65a5e66c3c22ded830e1d4f539e3bdbf40
Anargyros Tsadimas <tsadimas@gmail.com>
2016-01-13 21:37:53 +0200

    upload files, using http://www.journaldev.com/2573/spring-mvc-file-upload-example-tutorial-single-and-multiple-files

--- src/main/java/gr/hua/lab2/HomeController.java
+++ src/main/java/gr/hua/lab2/HomeController.java
@@ -1,6 +1,11 @@
 package gr.hua.lab2;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
@@ -19,10 +24,14 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import gr.hua.lab2.model.EmployeeDAO;
 import gr.hua.lab2.model.Employee;
+
 
 /**
  * Handles requests for the application home page.
@@ -80,4 +89,100 @@ public class HomeController {
 		employeeDAO.deleteById(id);
 		return "redirect:/";
 	}
-}
+	
+	
+	@RequestMapping(value = "/upload", method = RequestMethod.GET)
+	public String uploadfile() {
+		return "upload";
+	}
+	
+	@RequestMapping(value = "/multiupload", method = RequestMethod.GET)
+	public String multiuploadfile() {
+		return "uploadMultiple";
+	}
+	
+	/**
+     * Upload single file using Spring Controller
+     */
+    @RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
+    public @ResponseBody
+    String uploadFileHandler(@RequestParam("name") String name,
+            @RequestParam("file") MultipartFile file) {
+ 
+        if (!file.isEmpty()) {
+            try {
+                byte[] bytes = file.getBytes();
+ 
+                // Creating the directory to store file
+                String rootPath = System.getProperty("catalina.home");
+                File dir = new File(rootPath + File.separator + "tmpFiles");
+                System.out.println("Upload path "+dir );
+                if (!dir.exists())
+                    dir.mkdirs();
+ 
+                // Create the file on server
+                File serverFile = new File(dir.getAbsolutePath()
+                        + File.separator + name);
+                BufferedOutputStream stream = new BufferedOutputStream(
+                        new FileOutputStream(serverFile));
+                stream.write(bytes);
+                stream.close();
+ 
+                logger.info("Server File Location="
+                        + serverFile.getAbsolutePath());
+ 
+                return "You successfully uploaded file=" + name;
+            } catch (Exception e) {
+                return "You failed to upload " + name + " => " + e.getMessage();
+            }
+        } else {
+            return "You failed to upload " + name
+                    + " because the file was empty.";
+        }
+    }
+ 
+    /**
+     * Upload multiple file using Spring Controller
+     */
+    @RequestMapping(value = "/uploadMultipleFile", method = RequestMethod.POST)
+    public @ResponseBody
+    String uploadMultipleFileHandler(@RequestParam("name") String[] names,
+            @RequestParam("file") MultipartFile[] files) {
+ 
+        if (files.length != names.length)
+            return "Mandatory information missing";
+ 
+        String message = "";
+        for (int i = 0; i < files.length; i++) {
+            MultipartFile file = files[i];
+            String name = names[i];
+            try {
+                byte[] bytes = file.getBytes();
+ 
+                // Creating the directory to store file
+                String rootPath = System.getProperty("catalina.home");
+                File dir = new File(rootPath + File.separator + "tmpFiles");
+                if (!dir.exists())
+                    dir.mkdirs();
+ 
+                // Create the file on server
+                File serverFile = new File(dir.getAbsolutePath()
+                        + File.separator + name);
+                BufferedOutputStream stream = new BufferedOutputStream(
+                        new FileOutputStream(serverFile));
+                stream.write(bytes);
+                stream.close();
+ 
+                logger.info("Server File Location="
+                        + serverFile.getAbsolutePath());
+ 
+                message = message + "You successfully uploaded file=" + name
+                        + "<br />";
+            } catch (Exception e) {
+                return "You failed to upload " + name + " => " + e.getMessage();
+            }
+        }
+        return message;
+    }
+	
+	}
